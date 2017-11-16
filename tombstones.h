981b6c9c45f3842e87766285b2f8746cb97bb8bc
lubalesuan <lubavable@mail.ru>
2017-11-16 00:02:38 -0500

    starting to work on tombstones.h. finished constructors, will get started on operators

--- tombstones.h
+++ tombstones.h
@@ -12,21 +12,30 @@ template <class T> void free(Pointer<T>& obj);
 template <class T>
 class Pointer {
 public:
-    Pointer<T>();                               // default constructor
-    Pointer<T>(Pointer<T>&);                        // copy constructor
-    Pointer<T>(T* pointee) : pointee_ (pointee);                             // bootstrapping constructor
-        // argument should always be a call to new
+    Pointer<T>() {
+         // default constructor
+        //init self???
+    }                             
+    Pointer<T>(Pointer<T>& otherPointer) {// copy constructor
+        this->t = otherPointer->t; //CHECK
+    }                       
+    Pointer<T>(T* object) {// bootstrapping constructor, argument should always be a call to new
+        this->t = new Tombstone(object, 1);
+        //if pointer was pointed to some object x2 before, and x2 wasn't deleted
+        //do I throw error here???
+    }                           
     ~Pointer<T>();                              // destructor
     T& operator*() const {
-        return *pointee_;
+        return (t->pointerToObj)&; //CHECK
     }                // deferencing
     T* operator->() const {
-        return pointee_;
+        return t->pointerToObj; //CHECK
     }                  // field dereferencing
     Pointer<T>& operator=(const Pointer<T>&);       // assignment
     friend void free<T>(Pointer<T>&);           // delete pointed-at object
         // This is essentially the inverse of the new inside the call to
         // the bootstrapping constructor.
+    //free deletes the tombstone and object and pointer
     // equality comparisons:
     bool operator==(const Pointer<T>&) const;
     bool operator!=(const Pointer<T>&) const;
@@ -35,7 +44,7 @@ public:
     bool operator!=(const int) const;
         // false iff Pointer is null and int is zero
 private:
-    T* pointee_;
+    Tombstone t;
 };
 
 
