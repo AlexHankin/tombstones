12da2d5a0f0829e9e26e83b7ec1908c304827240
AlexHankin <ahankin@u.rochester.edu>
2017-11-17 16:03:13 -0500

    Fixed small syntax error in foo2

--- foo2.cpp
+++ foo2.cpp
@@ -21,20 +21,20 @@ int main(int argc, char **argv)
     Pointer<int> bar(foo);
 
     if (bar == 0)
-	Error("Bar should not be null!");
+	error("Bar should not be null!");
     if (*bar != 12)
-	Error("Bar got the wrong value!");
+	error("Bar got the wrong value!");
     if (foo != bar)
-	Error("Foo and bar are NOT distinct pointers!");
+	error("Foo and bar are NOT distinct pointers!");
     if (*foo != *bar)
-	Error("Foo and bar should have the same value here!");
+	error("Foo and bar should have the same value here!");
     *foo = 15;
     if (*bar != 15)
-	Error("Bar should still match foo!");
+	error("Bar should still match foo!");
 
     free(foo);
     free(bar);
-    Error("Attempt to double-delete pointer not flagged!");
+    error("Attempt to double-delete pointer not flagged!");
 
     return 0;
 }
